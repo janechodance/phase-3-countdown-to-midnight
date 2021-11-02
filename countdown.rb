b4817d6c3c76b72c441d9d62608afa9e383538ba
Jane <janechodance@gmail.com>
2021-11-01 23:51:33 -0400

    commit tree 6469fed1abd7da7a8cf526ef17dff50562a9b576

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+def countdown (number)
+    i=number
+    while i> 0
+    puts "#{i} SECOND(S)!"
+    i -=1
+    end
+    "HAPPY NEW YEAR!"
+end
+def countdown_with_sleep (number)
+    i=number
+    while i> 0
+    puts "#{i} SECOND(S)!"
+    i -=1
+    sleep(1)
+    end
+    "HAPPY NEW YEAR!"
 end
